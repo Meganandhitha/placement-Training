5f49b3b381abd9eca6a33fa731fa6132e073069f
Meganandhitha <151141835+Meganandhitha@users.noreply.github.com>
2024-07-31 22:09:58 +0530

    Create userdefined.py

---
+++ 31.07.2024/userdefined.py
@@ -0,0 +1,16 @@
+class NegativeNumberError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return f"A negative number {self.value} was encountered in the list."
+
+def check_list(numbers):
+    for num in numbers:
+        if num < 0:
+            raise NegativeNumberError(num)
+
+try:
+    check_list([1, 2, -3, 4])
+except NegativeNumberError as e:
+    print(e)
